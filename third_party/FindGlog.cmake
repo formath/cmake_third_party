793f9489fc91eff1598b0f14be57c633bef4b354
formath <jinpengliu@163.com>
2019-11-26 20:25:26 +0800

    fix

--- third_party/FindGlog.cmake
+++ third_party/FindGlog.cmake
@@ -109,10 +109,10 @@ macro(DO_FIND_GLOG_DOWNLOAD)
 		URL https://github.com/google/glog/archive/v${GLOG_REQUESTED_VERSION}.zip
 		URL_HASH SHA256=9e1b54eb2782f53cd8af107ecf08d2ab64b8d0dc2b7f5594472f3bd63ca85cdc
 		UPDATE_COMMAND ""
-		CONFIGURE_COMMAND ./autogen.sh && ./configure
+		CONFIGURE_COMMAND ./autogen.sh && ./configure --prefix=${GLOG_ROOT_DIR}
 		BUILD_COMMAND make
 		BUILD_IN_SOURCE true
-		INSTALL_COMMAND make install DESTDIR=${GLOG_ROOT_DIR}
+		INSTALL_COMMAND make install
 		INSTALL_DIR ${GLOG_ROOT_DIR}
 		)
 
